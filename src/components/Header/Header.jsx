de85bad23b2d3eb33697f7f30bf7e26ac1ae1466
tamatar <>
2021-04-30 11:30:38 +0530

    route chng logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,10 @@ function Header() {
     <div className="container-fluid">
       <div className="row">
         <div className="col-sm-3 p-0">
-          <Link to="/" className="logo text-decoration-none">
+          <Link
+            to="/flix-mania-rc-frontend/"
+            className="logo text-decoration-none"
+          >
             <h1 className="p-3 font-weight-bold">FLIX MANIA</h1>
           </Link>
         </div>
